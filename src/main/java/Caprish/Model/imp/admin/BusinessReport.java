3822400c0b5322b6bc0db10acd8abc4f5161bfc4
juanfranpaezz <juanfranciscopaezz@gmail.com>
2025-05-03 18:59:52 -0300

    ;)

--- src/main/java/Caprish/Model/imp/admin/BusinessReport.java
+++ src/main/java/Caprish/Model/imp/admin/BusinessReport.java
@@ -1,8 +1,34 @@
 package Caprish.Model.imp.admin;
 
 import Caprish.Model.imp.MyObjects;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name="BusinnesReport")
 public class BusinessReport extends MyObjects {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id_business_report;
+    @Column (nullable=false)
+    private Long id_business;
+    @Column (nullable=false)
+    private LocalDate generated_at;
+    @Column (columnDefinition= "TEXT")
+    private String description;
+    @NotBlank(message="aaa")
+    private String about;
+
     String sql =    "CREATE TABLE business (" +
                     "id_business_report BIGINT PRIMARY KEY AUTO_INCREMENT," +
                     "id_business INT NOT NULL," +

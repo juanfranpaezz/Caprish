e5a7f2103d2001c84f020331cd28bd934e5f3b89
juanfranpaezz <juanfranciscopaezz@gmail.com>
2025-05-03 19:24:56 -0300

    ;)

--- src/main/java/Caprish/Model/imp/admin/BusinessReport.java
+++ src/main/java/Caprish/Model/imp/admin/BusinessReport.java
@@ -1,9 +1,9 @@
 package Caprish.Model.imp.admin;
 
 import Caprish.Model.imp.MyObjects;
+import Caprish.Model.imp.business.Business;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,30 +12,28 @@ import java.time.LocalDate;
 
 @Entity
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
-@Table(name="BusinnesReport")
+@Table(name="businness_report")
 public class BusinessReport extends MyObjects {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id_business_report;
     @Column (nullable=false)
-    private Long id_business;
+    @ManyToOne
+    @JoinColumn(name = "id_business", nullable = false)  // FK en la tabla business_report
+    private Business id_business; //tiene que ser un objeto
     @Column (nullable=false)
     private LocalDate generated_at;
     @Column (columnDefinition= "TEXT")
     private String description;
-    @NotBlank(message="aaa")
+    @NotBlank(message="la descripcion no puede estar vacia")
     private String about;
 
-    String sql =    "CREATE TABLE business (" +
-                    "id_business_report BIGINT PRIMARY KEY AUTO_INCREMENT," +
-                    "id_business INT NOT NULL," +
-                    "generated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\n" +
-                    "CONSTRAINT id_business FOREIGN KEY (id_business)" +
-                    "REFERENCES business(id_business)" +
-                    "about VARCHAR(200), NOT NULL" +
-                    "description TEXT" +
-                    ");";
+    public BusinessReport(Business id_business, LocalDate generated_at, String description, String about) {
+        this.id_business = id_business;
+        this.generated_at = generated_at;
+        this.description = description;
+        this.about = about;
+    }
 }
--- src/main/java/Caprish/Model/imp/admin/ClientReport.java
+++ src/main/java/Caprish/Model/imp/admin/ClientReport.java
@@ -1,13 +1,33 @@
 package Caprish.Model.imp.admin;
 
+import Caprish.Model.imp.sales.Sale;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name="client_report")
 public class ClientReport {
-    String sql =    "CREATE TABLE client_report (\n" +
-                    "  id_client_report BIGINT PRIMARY KEY AUTO_INCREMENT,\n" +
-                    "  id_sale BIGINT,\n" +
-                    "  generated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\n" +
-                    "  report_data TEXT,\n" +
-                    "  FOREIGN KEY (id_sale)\n" +
-                    "    REFERENCES sale(id_sale)\n" +
-                    "    ON DELETE SET NULL\n" +
-                    ");";
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    @Column(name="id_sale" ,nullable = false)
+    private Sale id_sale;
+    @Column (nullable = false)
+    private LocalDate report_date;
+    @Column (name = "TEXT")
+    private String report_data;
+
+    public ClientReport(LocalDate report_date, Sale id_sale, String report_data) {
+        this.report_date = report_date;
+        this.id_sale = id_sale;
+        this.report_data = report_data;
+    }
 }

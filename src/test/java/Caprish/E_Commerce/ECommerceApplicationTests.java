d70e53e41f81179a0e621daff063456bda015adf
Mattew0800 <matiasoyhamburu24@gmail.com>
2025-05-03 16:58:59 -0300

    Subir Proyecto Spring Boot Test

---
+++ src/test/java/Caprish/E_Commerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package Caprish.E_Commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

42e3568c663efd824cb4259c7881d40e868e9186
juanfranpaezz <juanfranciscopaezz@gmail.com>
2025-05-03 21:33:37 -0300

    business actualizada

--- src/main/java/Caprish/Model/imp/business/Business.java
+++ src/main/java/Caprish/Model/imp/business/Business.java
@@ -1,7 +1,9 @@
 package Caprish.Model.imp.business;
 
 import Caprish.Model.imp.MyObjects;
+import Caprish.Model.imp.users.Staff;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,23 +18,32 @@ public class Business extends MyObjects {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_business;
-    @Column(unique=true)
+    @Column(unique=true,columnDefinition="TEXT",nullable=false)
+    private String bussiness_name;
+    @Column(nullable=false,columnDefinition = "TEXT")
+    private String description;
+    @Column(nullable = false,columnDefinition = "TEXT")
+    private String slogan;
+    @OneToOne
+    @JoinColumn(nullable = false,name="id_staff")
+    private Staff id_staff;
+    @Column(nullable = false,columnDefinition = "TEXT")
+    private String address;
+    @Column(nullable = false)
+    @NotBlank(message="El cuit no puede estar vacio")
+    private int tax;
 
 
-
-
-
-
-    String sql = "CREATE TABLE business (\n" +
-            "    id_business INT PRIMARY KEY AUTO_INCREMENT,\n" +
-            "    business_name VARCHAR(100) NOT NULL UNIQUE,\n" +
-            "    description TEXT NOT NULL,\n" +
-            "    slogan TEXT NOT NULL,\n" +
-            "    id_staff INT,\n" +
-            "    address VARCHAR(50) NOT NULL,\n" +
-            "    tax INT,\n" +
-            "    FOREIGN KEY (id_boss) REFERENCES bosses(id_boss)    \n" +
-            ");";
+//    String sql = "CREATE TABLE business (\n" +
+//            "    id_business INT PRIMARY KEY AUTO_INCREMENT,\n" +
+//            "    business_name VARCHAR(100) NOT NULL UNIQUE,\n" +
+//            "    description TEXT NOT NULL,\n" +
+//            "    slogan TEXT NOT NULL,\n" +
+//            "    id_staff INT,\n" +
+//            "    address VARCHAR(50) NOT NULL,\n" +
+//            "    tax INT,\n" +
+//            "    FOREIGN KEY (id_staff) REFERENCES bosses(id_staff)    \n" +
+//            ");";
 
 
 

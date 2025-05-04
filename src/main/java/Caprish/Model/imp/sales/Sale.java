8a80ac5de781da258be9fefa58ed4d5e0cf39549
juanfranpaezz <juanfranciscopaezz@gmail.com>
2025-05-03 22:27:08 -0300

    parte de sales actualizada

--- src/main/java/Caprish/Model/imp/sales/Sale.java
+++ src/main/java/Caprish/Model/imp/sales/Sale.java
@@ -1,16 +1,41 @@
 package Caprish.Model.imp.sales;
 
 import Caprish.Model.imp.MyObjects;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+
+@NoArgsConstructor
+@Entity
+@Table(name="sale")
 public class Sale  extends MyObjects {
+    @Id
+    @GeneratedValue (strategy= GenerationType.AUTO)
+    private Long id;
 
-    String sql =    "CREATE TABLE sale (\n" +
-                    "  id_sale        BIGINT AUTO_INCREMENT PRIMARY KEY,\n" +
-                    "  id_cart  BIGINT NOT NULL,\n" +
-                    "  sale_date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\n" +
-                    "  total_amount   DECIMAL(12,2) NOT NULL,\n" +
-                    "  FOREIGN KEY (id_cart) REFERENCES cart(id_cart) ON DELETE RESTRICT\n" +
-                    ");";
+    @OneToOne
+    @JoinColumn(name="id_cart")
+    private ShoppingCart id_cart;
+
+    @Column(nullable = false)
+    private LocalDate sale_date;
+
+    @Column(nullable = false,precision = 20, scale = 10)
+    private BigDecimal total_amount;
+
+
+//
+//    String sql =    "CREATE TABLE sale (\n" +
+//                    "  id_sale        BIGINT AUTO_INCREMENT PRIMARY KEY,\n" +
+//                    "  id_cart   BIGINT NOT NULL,\n" +
+//                    "  sale_date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\n" +
+//                    "  total_amount   DECIMAL(12,2) NOT NULL,\n" +
+//                    "  FOREIGN KEY (id_cart) REFERENCES cart(id_cart) ON DELETE RESTRICT\n" +
+//                    ");";
+
 }
 
 
--- src/main/java/Caprish/Model/imp/sales/SaleCart.java
+++ src/main/java/Caprish/Model/imp/sales/SaleCart.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
+
 @Table(name="cart")
 @NoArgsConstructor
 @Getter

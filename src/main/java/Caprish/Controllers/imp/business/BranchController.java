1f063fe603bbe3172b43cdfc7725c50c0f0f39a9
Nachoerbin <erbinignacio@gmail.com>
2025-05-29 14:50:04 -0300

    fix errors

--- src/main/java/Caprish/Controllers/imp/business/BranchController.java
+++ src/main/java/Caprish/Controllers/imp/business/BranchController.java
@@ -6,13 +6,13 @@ import Caprish.Repository.interfaces.business.BranchRepository;
 import Caprish.Service.imp.business.BranchService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/branch")
+//public class BranchController extends MyObjectGenericController<Branch, BranchRepository,BranchService> {
+//
+//    public BranchController(BranchService service) {
+//        super(service);
+//    }
 
-@RestController
-@RequestMapping("/branch")
-public class BranchController extends MyObjectGenericController<Branch, BranchRepository, BranchService> {
-
-    public BranchController(BranchService service) {
-        super(service);
-    }
-
-}
+//}
--- src/main/java/Caprish/Service/imp/business/ProductService.java
+++ src/main/java/Caprish/Service/imp/business/ProductService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 
 @Service
-public class ProductService extends MyObjectGenericService<Product, ProductRepository> {
+public class ProductService extends MyObjectGenericService<Product, ProductRepository, ProductService> {
 
     @Autowired
     ProductRepository productRepository;

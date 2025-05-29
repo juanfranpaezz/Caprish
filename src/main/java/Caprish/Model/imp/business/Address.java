22ea444a417da8e18cdafc6f898460157f01a257
Mattew0800 <149548441+Mattew0800@users.noreply.github.com>
2025-05-29 15:33:06 -0300

    Update Address.java

--- src/main/java/Caprish/Model/imp/business/Address.java
+++ src/main/java/Caprish/Model/imp/business/Address.java
@@ -1,7 +1,28 @@
 package Caprish.Model.imp.business;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Address {
-    public Address() {
+
+    @NotBlank(message = "La dirección no puede estar vacía")
+    private String direccion;
+
+    @NotBlank(message = "La localidad no puede estar vacía")
+    private String localidad;
+
+    @NotBlank(message = "La provincia no puede estar vacía")
+    private String provincia;
+
+    @NotBlank(message = "El país no puede estar vacío")
+    private String pais;
+
+    public String getFullAddress() {
+        return direccion + ", " + localidad + ", " + provincia + ", " + pais;
     }
 }
-

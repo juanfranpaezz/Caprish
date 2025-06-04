7ea20fceed34abfeadd42bde3baf126099007fff
Mattew0800 <matiasoyhamburu24@gmail.com>
2025-06-04 11:15:05 -0300

    Partial merge from develop-task-08: selectively incorporated updates in CartService.java

--- src/main/java/Caprish/Service/imp/sales/CartService.java
+++ src/main/java/Caprish/Service/imp/sales/CartService.java
@@ -1,16 +1,34 @@
 package Caprish.Service.imp.sales;
 
-import Caprish.Model.imp.admin.BusinessReport;
+
 import Caprish.Model.imp.sales.Cart;
+import Caprish.Model.imp.sales.dto.CartViewDTO;
 import Caprish.Repository.interfaces.sales.CartRepository;
 import Caprish.Service.imp.MyObjectGenericService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-@Slf4j
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CartService extends MyObjectGenericService<Cart, CartRepository, CartService> {
     protected CartService(CartRepository childRepository) {
         super(childRepository);
+    }
+
+    public List<CartViewDTO> getCartViewsByBusiness(Long idBusiness) {
+        return repository.getCartViewsByBusinessId(idBusiness)
+                .stream()
+                .map(obj -> new CartViewDTO(
+                        ((Number) obj[0]).longValue(),             // idCart
+                        (String) obj[1],                           // clientName (first + last)
+                        (String) obj[2],                           // cartType
+                        (String) obj[3],                           // cartStatus
+                        (String) obj[4],                           // staffName (first + last)
+                        ((Number) obj[5]).longValue(),             // idBusiness
+                        ((Number) obj[6]).doubleValue()            // totalAmount
+                ))
+                .collect(Collectors.toList());
     }
 
     @Override
